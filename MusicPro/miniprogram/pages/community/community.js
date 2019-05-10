7565813b047cd1d63c1eff0942946f14ea92e20e
belowthetree <1363558876@qq.com>
2019-05-10 23:54:03 +0800

    添加底部栏，添加了几个图标

--- MusicPro/miniprogram/pages/community/community.js
+++ MusicPro/miniprogram/pages/community/community.js
@@ -1,4 +1,4 @@
-// miniprogram/pages/community/community.js
+// pages/test2/test2.js
 Page({
 
   /**
@@ -6,10 +6,77 @@ Page({
    */
   data: {
 
-  },
 
-  test:function(e){
-    
+    moment: [
+      {
+        id: 0,
+        pride: 2,
+        username: "小明",
+        userurl: 'https://p1.music.126.net/zF9HHBntAoqM_DJsEJkeDA==/109951164027525982.jpg',
+        intro: "小可分享了《春歌》",
+        content:"每一次看这歌词，就会让我想起自己的故事",
+      },
+      {
+        id: 1,
+        pride: 2,
+        username: "小hong",
+        userurl: 'https://p1.music.126.net/I1YJ0IxMFA2T4axSB8x_ww==/109951164026094675.jpg',
+        intro: "小可分享了《春歌》",
+        content: "这种淡淡的旋律直击心灵，难以忘怀",
+      },
+      {
+        id: 2,
+        pride: 2,
+        username: "小明",
+        userurl: 'https://p1.music.126.net/HKas-jZi5vtiRA5zHdREzw==/109951163989475693.jpg',
+        intro: "小可分享了《春歌》",
+        content: "每一次看这歌词，就会让我想起自己的故事",
+      },
+      {
+        id: 3,
+        pride: 2,
+        username: "小明",
+        userurl: 'https://p1.music.126.net/HKas-jZi5vtiRA5zHdREzw==/109951163989475693.jpg',
+        intro: "小可分享了《春歌》",
+        content: "每一次看这歌词，就会让我想起自己的故事",
+      }, {
+        id: 4,
+        pride: 2,
+        username: "小明",
+        userurl: 'https://p1.music.126.net/HKas-jZi5vtiRA5zHdREzw==/109951163989475693.jpg',
+        intro: "小可分享了《春歌》",
+        content: "每一次看这歌词，就会让我想起自己的故事",
+      }, {
+        id: 5,
+        pride: 2,
+        username: "小明",
+        userurl: 'https://p1.music.126.net/HKas-jZi5vtiRA5zHdREzw==/109951163989475693.jpg',
+        intro: "小可分享了《春歌》",
+        content: "每一次看这歌词，就会让我想起自己的故事",
+      }, {
+        id: 5,
+        pride: 2,
+        username: "小明",
+        userurl: 'https://p1.music.126.net/HKas-jZi5vtiRA5zHdREzw==/109951163989475693.jpg',
+        intro: "小可分享了《春歌》",
+        content: "每一次看这歌词，就会让我想起自己的故事",
+      }, {
+        id: 6,
+        pride: 2,
+        username: "小明",
+        userurl: 'https://p1.music.126.net/HKas-jZi5vtiRA5zHdREzw==/109951163989475693.jpg',
+        intro: "小可分享了《春歌》",
+        content: "每一次看这歌词，就会让我想起自己的故事",
+      }, {
+        id: 7,
+        pride: 2,
+        username: "小明",
+        userurl: 'https://p1.music.126.net/HKas-jZi5vtiRA5zHdREzw==/109951163989475693.jpg',
+        intro: "小可分享了《春歌》",
+        content: "每一次看这歌词，就会让我想起自己的故事",
+      },
+    ]
+
   },
 
   /**
