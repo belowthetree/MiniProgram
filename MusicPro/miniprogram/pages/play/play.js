a82d0794f7baeed839362e7c0f4598265faef321
xiaoxiannu <1047092413@qq.com>
2019-05-17 00:11:49 +0800

    Update play.js

--- MusicPro/miniprogram/pages/play/play.js
+++ MusicPro/miniprogram/pages/play/play.js
@@ -1,248 +1,250 @@
-// pages/play/play.js
-var app=getApp()
+var app = getApp()
 const innerAudioContext = wx.createInnerAudioContext()
 const bgm = wx.getBackgroundAudioManager();
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    onplay: true,
-    order:1,
-    // time_song:app.data.songlist[app.data.songIndex].time
+    name:'',
+    singer:'',
+    song: null,
+    /**歌词 有关部分*/
+    animation: '',
+    isplayed: true, //是否播放
+    iscollect: true, // 是否收藏
+    isorder: false,  // 是否乱序
+    
   },
-   addLike: function () {
-    wx.cloud.callFunction({
-      name: "updataLike",
-      data: {
-        songname: app.data.songlist[app.data.songIndex].name,
-        songurl: app.data.songlist[app.data.songIndex].url,
-        singer: app.data.songlist[app.data.songIndex].singer,
-        songpic: app.data.songlist[app.data.songIndex].pic
-
-      },
-      success: function (res) {
-        console.log(res)
-        wx.showToast({
-          title: '添加成功',
-          duration: 1000,
-          mask: true
-        })
-      },
-      fail: function (res) {
-        console.log(res)
-      }
-    })
+  //收藏音乐图标
+  collectmusic: function () {
+    if (this.data.iscollect) {
+      this.setData({
+        iscollect: false
+      })
+    } else {
+      this.setData({
+        iscollect: true
+      })
+    }
   },
 
-
-  playAndtime: function(){
+  playAndtime: function () {
     var that = this;
     innerAudioContext.autoplay = true;
     innerAudioContext.stop();
     innerAudioContext.src = app.data.songlist[app.data.songIndex].url;
     innerAudioContext.play(() => {
-    })
-    innerAudioContext.onTimeUpdate(() => {
-      var duration = innerAudioContext.duration;
-      var offset = innerAudioContext.currentTime;
-      var currentTime = parseInt(innerAudioContext.currentTime);
-      var min = "0" + parseInt(currentTime / 60);
-      var max = parseInt(innerAudioContext.duration);
-      var sec = currentTime % 60;
-      if (sec < 10) {
-        sec = "0" + sec;
-      };
-      var starttime = min + ':' + sec;   /*  00:00  */
       that.setData({
-        offset: currentTime,
-        starttime: starttime,
-        max: max,
-        changePlay: true
+        song:app.data.songlist[app.data.songIndex]
       })
     })
+    if (getCurrentPages().length != 0) {
+      //刷新当前页面的数据
+      getCurrentPages()[getCurrentPages().length - 1].onLoad()
+    }
   },
 
 
-  pauseSong: function () {
-    if(this.data.onplay){
-    innerAudioContext.pause();
-    this.data.onplay=false;
+  //暂停播放按钮
+  listenerButtonPlay: function () {
+    console.log(this.data.song)
+    if (this.data.isplayed) {
+      innerAudioContext.pause();
+      this.setData({ isplayed: false})
     }
-    else{
+    else {
       innerAudioContext.play();
-      this.data.onplay=true;
+      this.setData({ isplayed: true })
     }
   },
 
+  lastSong: function () {
 
-  lastSong: function(){
-  
-    if(app.data.songIndex)
-    {
+    if (app.data.songIndex) {
       app.data.songIndex--;
       this.playAndtime()
-   }
-   else{
-     
-     this.data.onplay=false;
+    }
+    else {
+
+      this.data.isplayed = false;
       wx.showToast({
         title: '没有更多歌曲了',
         duration: 1000,
         mask: true
       })
-   }
+    }
 
   },
 
-  nextSong: function(){
-    if(app.data.songIndex==app.data.songlist.length-1){
+  nextSong: function () {
+    if (app.data.songIndex == app.data.songlist.length - 1) {
       innerAudioContext.stop();
-      this.data.onplay = false;
-       wx.showToast({
+      this.data.isplayed = false;
+      wx.showToast({
         title: '没有更多歌曲了',
         duration: 1000,
         mask: true
       })
     }
-    else{
+    else {
       app.data.songIndex++;
       this.playAndtime();
     }
   },
- 
-  
 
- choseOrder: function(){
-   var that=this;
-   that.setData({
-     order:++that.data.order
-   })
-   if(that.data.order%3==0){
-     innerAudioContext.onEnded(() => {
-       that.setData({ onplay: true })
-       if (app.data.songIndex == app.data.songlist.length - 1) {
-         innerAudioContext.stop();
-         this.data.onplay = false;
-         wx.showToast({
-           title: '没有更多歌曲了',
-           duration: 1000,
-           mask: true
-         })
-       }
-       else {
-         app.data.songIndex++;
-       }
-     })
-   }
-   else{
-     if(that.data.order%3 == 1){
-       app.data.songIndex = parseInt((Math.random() * (app.data.songlist.length)));
-      
-     }
-     else{
-      innerAudioContext.loop=true;
-     }
-   }
- },
+
+  //顺序播放按钮
+  playorder: function () {
+    if (this.data.isorder) {
+      innerAudioContext.onEnded(() => {
+        that.setData({ isplayed: true })
+        if (app.data.songIndex == app.data.songlist.length - 1) {
+          innerAudioContext.stop();
+          this.data.isplayed = false;
+          wx.showToast({
+            title: '没有更多歌曲了',
+            duration: 1000,
+            mask: true
+          })
+        }
+        else {
+          app.data.songIndex++;
+        }
+      })
+      this.setData({
+        isorder: false
+      })
+    } else {
+      app.data.songIndex = parseInt((Math.random() * (app.data.songlist.length)));
+      this.setData({
+        isorder: true
+      })
+    }
+
+  },
+    // 页面卸载时停止播放
+    onUnload() {
+    
+  },
+
+   
+    
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+    var that = this;
+    that.setData({
+      song:app.data.songlist[app.data.songIndex],
   
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (option) {
-      var that = this;
-  
-      innerAudioContext.autoplay = true;
-      innerAudioContext.src = app.data.songlist[app.data.songIndex].url;
+    })
+    innerAudioContext.autoplay = true;
+    innerAudioContext.src = app.data.songlist[app.data.songIndex].url;
+    innerAudioContext.play(() => {
+    })
+    innerAudioContext.onTimeUpdate(() => {
+      var time_now = innerAudioContext.currentTime;
+    })
+    innerAudioContext.onEnded(() => {
+      that.setData({ isplayed: true })
+      if (app.data.songIndex == app.data.songlist.length - 1) {
+        innerAudioContext.stop();
+        this.data.isplayed = false;
+        wx.showToast({
+          title: '没有更多歌曲了',
+          duration: 1000,
+          mask: true
+        })
+      }
+      else {
+        app.data.songIndex++;
+        innerAudioContext.stop();
+        innerAudioContext.src = app.data.songlist[app.data.songIndex].url;
         innerAudioContext.play(() => {
         })
-    innerAudioContext.onTimeUpdate(() => {
-      var time_now=innerAudioContext.currentTime;
+      }
     })
-       innerAudioContext.onEnded(()=>{
-         that.setData({onplay:true})
-         if (app.data.songIndex == app.data.songlist.length - 1) {
-           innerAudioContext.stop();
-           this.data.onplay = false;
-           wx.showToast({
-             title: '没有更多歌曲了',
-             duration: 1000,
-             mask: true
-           })
-         }
-         else{
-         app.data.songIndex++;
-         innerAudioContext.stop();
-         innerAudioContext.src = app.data.songlist[app.data.songIndex].url;
-         innerAudioContext.play(() => {
-         })
-         }
-       })
-      innerAudioContext.onError((res) => {
-        console.log(res.errMsg)
-        console.log(res.errCode)
+    innerAudioContext.onError((res) => {
+      console.log(res.errMsg)
+      console.log(res.errCode)
+    })
+
+  },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+      this.animation = wx.createAnimation({
+        duration: 1400,
+        timingFunction: 'linear', // "linear","ease","ease-in","ease-in-out","ease-out","step-start","step-end"
+        delay: 0,
+        transformOrigin: '50% 50% 0',
+        success: function (res) {
+          console.log("res")
+        }
       })
+    },
+    rotateAni: function (n) {
+      console.log("rotate==" + n)
+      this.animation.rotate(180 * (n)).step()
+      this.setData({
+        animation: this.animation.export()
+      })
+    },
 
-  },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
 
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
     
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    var time = innerAudioContext.currentTime;
-    innerAudioContext.destroy();
    
    
-    bgm.start = time;
-    bgm.title = app.data.songlist[app.data.songIndex].name;
-    bgm.coverImgUrl = app.data.songlist[app.data.songIndex].pic;
-    bgm.src = app.data.songlist[app.data.songIndex].url;
-    
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+      var time = innerAudioContext.currentTime;
+      innerAudioContext.destroy();
+      bgm.startTime = time;
+      bgm.title = app.data.songlist[app.data.songIndex].name;
+      bgm.coverImgUrl = app.data.songlist[app.data.songIndex].pic;
+      bgm.src = app.data.songlist[app.data.songIndex].url;
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
   
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    var time = innerAudioContext.currentTime;
-    innerAudioContext.destroy();
-    bgm.startTime = time;
-    console.log(time);
-    bgm.title = app.data.songlist[app.data.songIndex].name;
-    bgm.coverImgUrl = app.data.songlist[app.data.songIndex].pic;
-    bgm.src = app.data.songlist[app.data.songIndex].url;
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
